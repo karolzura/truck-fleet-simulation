3b084c42d41a356ba5172f7303d72e35231a35d7
Karol <karolzuranski7@gmail.com>
2026-04-12 10:37:38 +0200

    fix: add deadline for vrp logic

--- core/include/Destination.h
+++ core/include/Destination.h
@@ -5,13 +5,15 @@
 
 struct Destination {
     std::string name;
+    std::string order_id;
     float x, y;
     int priority;
+    long long deadline_ts;
     bool visited = false;
     int skip_count = 0;
 
-    Destination(std::string n, float _x, float _y, int p) 
-        : name(n), x(_x), y(_y), priority(p) {}
+    Destination(std::string n, std::string oid, float _x, float _y, int p, long long dl)
+        : name(n), order_id(oid), x(_x), y(_y), priority(p), deadline_ts(dl) {}
 };
 
 #endif
